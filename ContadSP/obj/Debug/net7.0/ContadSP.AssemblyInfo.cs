409f4b46f656a05a865dd9ee817b4d1e5e891b74
Fernando Francisco Asmuzi <fasmuzi@podjudsp.local>
2024-09-06 18:30:05 -0300

    cambios al 06/09/2024

--- ContadSP/obj/Debug/net7.0/ContadSP.AssemblyInfo.cs
+++ ContadSP/obj/Debug/net7.0/ContadSP.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContadSP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+682cd4a352576b568e7e3d98bcd133e22fa5dd4c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5527c4da276b6a8059a51d2ad9790f932301900")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContadSP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContadSP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
